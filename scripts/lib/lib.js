9d97eac41f948abe89638186ccef9d35eacefd12
Faizan Ahmad <cfaizana@gmail.com>
2021-07-23 21:33:08 -0400

    Version 1 release

---
+++ scripts/lib/lib.js
@@ -0,0 +1,139 @@
+function HurtShake(shakeDelay, bloodOnHurt, woundSize, shakeLoops, shakeLoopDuration)
+{
+  console.log("Hurt Shaking");
+  let bloodEffect = 
+  {
+    filterType: "splash",
+    filterId: "wound",
+    rank:5,
+    color: 0x990505,
+    padding: 80,
+    time: Math.random()*1000,
+    seed: Math.random(),
+    splashFactor: 1,
+    spread: woundSize,
+    blend: 1,
+    dimX: 1,
+    dimY: 1,
+    cut: false,
+    textureAlphaBlend: true,
+    anchorX: 0.32+(Math.random()*0.36),
+    anchorY: 0.32+(Math.random()*0.36)
+  }
+  let hurtEffect =
+  [{
+      filterType: "transform",
+      filterId: "hurtShake",
+      autoDestroy: true,
+      padding: 80,
+      animated:
+      {
+          translationX:
+          {
+              animType: "cosOscillation",
+              val1: 0,
+              val2: -0.05,
+              loops: shakeLoops,
+              loopDuration: shakeLoopDuration
+          }
+      }
+  }];
+  if (bloodOnHurt)
+  {
+    hurtEffect.push(bloodEffect);
+  }
+  setTimeout(function(){
+    TokenMagic.addFiltersOnTargeted(hurtEffect);
+  },shakeDelay);
+}
+
+function DeathShake(shakeDelay, bloodOnDeath, shakeLoops, shakeLoopDuration, bloodEffectDelay)
+{
+  console.log("Death Shaking");
+  let deathEffect =
+  [{
+      filterType: "transform",
+      filterId: "hurtShake",
+      autoDestroy: true,
+      padding: 80,
+      animated:
+      {
+          translationX:
+          {
+              animType: "cosOscillation",
+              val1: 0,
+              val2: -0.05,
+              loops: shakeLoops,
+              loopDuration: shakeLoopDuration
+          }
+      }
+  }];
+  let bloodEffect =
+  [{
+      filterType: "splash",
+      filterId: "death",
+      color: 0x900505,
+      padding: 30,
+      time: Math.random()*1000,
+      seed: Math.random()/100,
+      splashFactor: 2,
+      spread: 7,
+      blend: 1,
+      dimX: 1,
+      dimY: 1,
+      cut: true,
+      textureAlphaBlend: false
+  }];
+  setTimeout(function(){
+    TokenMagic.addFiltersOnTargeted(deathEffect);
+    if(bloodOnDeath)
+    {
+      setTimeout(function(){
+        TokenMagic.addFiltersOnTargeted(bloodEffect, true);
+      },bloodEffectDelay);
+    }   
+  },shakeDelay);
+}
+
+Hooks.on("midi-qol.RollComplete", function(data){
+  if(data.hitTargets.size > 0)
+  {
+    let defaultDelay = game.settings.get("TargetReacts","defaultShakeDelay");
+    let bloodOnHurt = game.settings.get("TargetReacts","bloodOnHurt");
+    let bloodOnDeath = game.settings.get("TargetReacts","bloodOnDeath");
+    let shakeDelay = getProperty(data.item, "data.flags.TargetReacts.shakeDelay");
+    let hurtShakeLoops = game.settings.get("TargetReacts","hurtShakeLoops");
+    let hurtShakeLoopTime = game.settings.get("TargetReacts","hurtShakeLoopTime");
+    let deathShakeLoops = game.settings.get("TargetReacts","deathShakeLoops");
+    let deathShakeLoopTime = game.settings.get("TargetReacts","deathShakeLoopTime");
+    let bloodEffectDelay = game.settings.get("TargetReacts","deathBloodDelay");
+
+    let hpDamage = data.damageList[0].hpDamage;
+    let newHP = data.damageList[0].newHP;
+    let targetMaxHP = [...data.hitTargets][0].document._actor.data.data.attributes.hp.max;
+    let damageTotal = data.damageTotal;
+    let HpLost = (damageTotal/targetMaxHP) * 100;
+    let woundSize = Math.abs((0.02*HpLost) + 0.4);
+
+    if (shakeDelay == "")
+    {
+      shakeDelay = defaultDelay;
+    }
+    if (hpDamage > 0 && newHP > 0)
+    {
+      HurtShake(shakeDelay, bloodOnHurt, woundSize, hurtShakeLoops, hurtShakeLoopTime);
+    }
+    else if (hpDamage > 0 && newHP <= 0)
+    {
+      DeathShake(shakeDelay, bloodOnDeath, deathShakeLoops, deathShakeLoopTime, bloodEffectDelay);
+    }
+  }
+});
+
+Hooks.on("renderItemSheet", (app, html, data) => {
+  const element = html.find('input[name="data.chatFlavor"]').parent().parent();
+  const labelText = "Target Shake Delay";
+  const shakeDelay = getProperty(app.object.data, "flags.TargetReacts.shakeDelay") ?? "";
+  const shakeDelayField = `<div class="form-group"><label>${labelText}</label><input type="text" name="flags.TargetReacts.shakeDelay" value="${shakeDelay}"/> </div>`;
+  element.append(shakeDelayField); 
+});
--- scripts/module.js
+++ scripts/module.js
@@ -1,5 +1,99 @@
 Hooks.once('init', async function() {
-
+    console.log("Registering TargetReacts game settings...");
+    game.settings.register("TargetReacts", "defaultShakeDelay", {  
+        name: "Default Shake Delay",                  
+        hint: "Set default delay for hurt shake",               
+        scope: "world",                                     
+        config: true,                                      
+        type: Number,
+        range: {
+        min: 0,
+        max: 5000,
+        step: 25,
+        },
+        default: 1750,                                    
+    });
+    game.settings.register("TargetReacts", "bloodOnHurt", {  
+        name: "Blood when hurt?",                  
+        hint: "Apply the hurt blood splatter when target loses hp on hit?",               
+        scope: "world",                                     
+        config: true,                                      
+        type: Boolean,
+        default: true,                                    
+    });
+    game.settings.register("TargetReacts", "bloodOnDeath", {  
+        name: "Blood when death?",                  
+        hint: "Apply the death blood splatter when target dies on hit?",               
+        scope: "world",                                     
+        config: true,                                      
+        type: Boolean,
+        default: true,                                    
+    });
+    game.settings.register("TargetReacts", "hurtShakeLoops", {  
+        name: "Hurt Shake Loops",                  
+        hint: "Set how many times to shake when hurt",               
+        scope: "world",                                     
+        config: true,                                      
+        type: Number,
+        range: {
+        min: 1,
+        max: 5,
+        step: 1,
+        },
+        default: 2,                                    
+    });
+    game.settings.register("TargetReacts", "hurtShakeLoopTime", {  
+        name: "Hurt Shake Loop Time",                  
+        hint: "Set how long the shakes when hurt should take",               
+        scope: "world",                                     
+        config: true,                                      
+        type: Number,
+        range: {
+        min: 50,
+        max: 1000,
+        step: 25,
+        },
+        default: 150,                                    
+    });
+    game.settings.register("TargetReacts", "deathShakeLoops", {  
+        name: "Death Shake Loops",                  
+        hint: "Set how many times to shake on death",               
+        scope: "world",                                     
+        config: true,                                      
+        type: Number,
+        range: {
+        min: 1,
+        max: 5,
+        step: 1,
+        },
+        default: 4,                                    
+    });
+    game.settings.register("TargetReacts", "deathShakeLoopTime", {  
+        name: "Death Shake Loop Time",                  
+        hint: "Set how long the shakes on death should take",               
+        scope: "world",                                     
+        config: true,                                      
+        type: Number,
+        range: {
+        min: 50,
+        max: 1000,
+        step: 25,
+        },
+        default: 150,                                    
+    });
+    game.settings.register("TargetReacts", "deathBloodDelay", {  
+        name: "Death Blood Effect Delay",                  
+        hint: "Set how long to delay the blood splat on death",               
+        scope: "world",                                     
+        config: true,                                      
+        type: Number,
+        range: {
+        min: 0,
+        max: 5000,
+        step: 50,
+        },
+        default: 400,                                    
+    });
 });
 
 Hooks.once('ready', async function() {
